084f63afbe2314b4d50630668a85580639d1d3d5
ofranco1205-7409 <60754342+ofranco1205-7409@users.noreply.github.com>
2023-08-31 18:02:24 -0600

    Fin s8

--- 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.spec.ts
+++ 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.spec.ts
@@ -6,10 +6,12 @@ describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [SidebarComponent]
-    });
+    })
+    .compileComponents();
+
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
